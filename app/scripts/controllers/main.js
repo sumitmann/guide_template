ea2be6a621bec263644ff6a87e066add59048c96
Gaurav Kumar <gaurav@redmart.com>
2016-10-25 22:34:38 +0530

    angular template addeD

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name guideTemplateApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the guideTemplateApp
+ */
+angular.module('guideTemplateApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
